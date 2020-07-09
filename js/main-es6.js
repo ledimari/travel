fc912addb8e94573f42101c0cabe50e7b72053e3
ledimari <ledi.huliganka@gmail.com>
2020-07-09 18:01:45 +0300

    оптимизация изображений, фикс меню

--- js/main-es6.js
+++ js/main-es6.js
@@ -18,14 +18,20 @@
   const burgerItem = document.querySelector('.burger');
   const menu = document.querySelector('.header_nav');
   const close = document.querySelector('.header_nav-close');
+  const menuLinks = document.querySelectorAll('.header_link');
   burgerItem.addEventListener('click', () => {
     menu.classList.add('header_nav_active');
   });
   close.addEventListener('click', () => {
     menu.classList.remove('header_nav_active')
   });
-
-
+  if (window.innerWidth <= 767) {
+    for (let i = 0; i < menuLinks.length; i +=1) {
+      menuLinks[i].addEventListener('click', () => {
+        menu.classList.remove('header_nav_active');
+      });
+    }
+  }
 }());
 
 
